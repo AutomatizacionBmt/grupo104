8ec51bbbaa0555395f26646f16c71fbb1acd3348
Giancarlo <giancarlo2709@gmail.com>
2020-10-06 21:35:41 -0500

    add test for obtenerSalario

--- src/test/java/sesion01/CalculaSalarioBrutoTest.java
+++ src/test/java/sesion01/CalculaSalarioBrutoTest.java
@@ -1,4 +1,16 @@
 package sesion01;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CalculaSalarioBrutoTest {
+
+    @Test
+    public void obtenerSalarioTest() {
+        Double salarioActual = CalculaSalarioBruto.obtenerSalario(8, 12);
+        Double salarioEsperado = 96.0;
+
+        Assert.assertEquals("Salario incorrecto", salarioEsperado, salarioActual);
+    }
+
 }
