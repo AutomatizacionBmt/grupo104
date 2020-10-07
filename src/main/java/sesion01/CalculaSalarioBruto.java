d78897baa2ad11a98f5963fd1c0a4adbbb4c3254
Giancarlo <giancarlo2709@gmail.com>
2020-10-06 21:00:07 -0500

    add CalculaSalarioBruto

--- src/main/java/sesion01/CalculaSalarioBruto.java
+++ src/main/java/sesion01/CalculaSalarioBruto.java
@@ -1,4 +1,16 @@
 package sesion01;
 
+/*
+1. El primer paso es que obtendríamos la cantidad de horas trabajadas.
+2. Luego, obtenemos la tarifa de pago por hora.
+3. Luego, multiplicamos las horas y la tasa de pago.
+4. Finalmente, mostraríamos el resultado.
+ */
 public class CalculaSalarioBruto {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, Bienvenidos al Modulo de Java For Testing");
+        System.out.println("Hello world!");
+    }
+
 }
