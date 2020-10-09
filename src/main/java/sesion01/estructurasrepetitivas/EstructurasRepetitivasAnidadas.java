14864ec2557a0ff4f7b51b74fa0f31c568721ce0
Giancarlo <giancarlo2709@gmail.com>
2020-10-08 21:55:52 -0500

    estructuras repetitivas anidadas

---
+++ src/main/java/sesion01/estructurasrepetitivas/EstructurasRepetitivasAnidadas.java
@@ -0,0 +1,18 @@
+package sesion01.estructurasrepetitivas;
+
+public class EstructurasRepetitivasAnidadas {
+
+    public static void main(String[] args) {
+        for(int i =0; i <= 3 ; i++) { // 0,1,2,3
+            System.out.println("cuando i es:" + i);
+           for(int j=0; j <= 5; j++) { // 0,1,2,3,4,5
+               System.out.println("cuando j es: " + j);
+               int count = 0;
+               while(count < 2) {
+                   System.out.println(count);
+                   count++;
+               }
+           }
+        }
+    }
+}
