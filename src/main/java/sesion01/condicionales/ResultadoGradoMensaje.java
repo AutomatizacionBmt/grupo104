d3ad9e35d4eafeec8c05ebce01c92315a112a4fd
Giancarlo <giancarlo2709@gmail.com>
2020-10-08 20:53:34 -0500

    add condicionales switch

---
+++ src/main/java/sesion01/condicionales/ResultadoGradoMensaje.java
@@ -0,0 +1,36 @@
+package sesion01.condicionales;
+
+import java.util.Scanner;
+
+public class ResultadoGradoMensaje {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Ingrese el grado al que pertenece");
+        String grado = scanner.next();
+
+        String mensaje = "";
+
+        switch (grado) {
+            case "A" :
+                mensaje = "¡Excelente Trabajo, su puntaje es mayor de 90";
+                break;
+            case "B":
+                mensaje = "¡Gran Trabajo, su puntaje es menor a 90 y mayor a 80";
+                break;
+            case "C":
+                mensaje = "¡Buen Trabajo!, su puntaje es menor a 80 y mayor a 70";
+            case "D":
+                mensaje = "¡Necesitas esforzarte un poco más, su puntaje es menor a 70";
+                break;
+            case "E":
+                mensaje = "¡No puede ser!, su puntaje es menor a 60";
+                break;
+            default:
+                mensaje = "grado incorrecto";
+        }
+
+        System.out.println(mensaje);
+    }
+}
