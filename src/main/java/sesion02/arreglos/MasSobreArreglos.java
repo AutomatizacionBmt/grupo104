fda73cbea3587c3f5f2dbf32ae10819339fcae09
Giancarlo <giancarlo2709@gmail.com>
2020-10-15 21:49:03 -0500

    refactor

--- src/main/java/sesion02/arreglos/MasSobreArreglos.java
+++ src/main/java/sesion02/arreglos/MasSobreArreglos.java
@@ -9,18 +9,22 @@ public class MasSobreArreglos {
 
         Random random = new Random();
 
+        // Llenar la matriz
         for(int i = 0 ; i < 2; i++) {
             for(int j = 0; j < 3; j++) {
                 enteros[i][j] = random.nextInt(10) + 1;
-            }
-        }
-
-        for(int i = 0 ; i < 2; i++) {
-            for(int j = 0; j < 3; j++) {
                 System.out.print(enteros[i][j] + " | ");
             }
             System.out.println();
         }
+
+        // imprimir los valores de la matriz
+        /*for(int i = 0 ; i < 2; i++) {
+            for(int j = 0; j < 3; j++) {
+                System.out.print(enteros[i][j] + " | ");
+            }
+            System.out.println();
+        }*/
 
     }
 }
