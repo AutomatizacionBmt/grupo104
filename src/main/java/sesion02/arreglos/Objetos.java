2484b6f26448c3a4f5564b8c2cad4c193a029b82
Giancarlo <giancarlo2709@gmail.com>
2020-10-15 21:39:50 -0500

    en segunda forma

--- src/main/java/sesion02/arreglos/Objetos.java
+++ src/main/java/sesion02/arreglos/Objetos.java
@@ -5,9 +5,14 @@ import sesion02.clases.Usuario;
 public class Objetos {
 
     public static void main(String[] args) {
-        Usuario[] usuarios = new Usuario[2];
+        /*Usuario[] usuarios = new Usuario[2];
         usuarios[0] = new Usuario("Giancarlo", "Yarlequé", 28);
-        usuarios[1] = new Usuario("Elvis", "Juarez", 29);
+        usuarios[1] = new Usuario("Elvis", "Juarez", 29);*/
+
+        Usuario[] usuarios = {
+                new Usuario("Giancarlo", "Yarlequé", 28),
+                new Usuario("Elvis", "Juarez", 29)
+        };
 
         for(Usuario miUsuario : usuarios) {
             System.out.println(miUsuario);
