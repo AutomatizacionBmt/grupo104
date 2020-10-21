076c589f016fbacb3d9d0e2f71d30e1e1f7eb0dc
Giancarlo <giancarlo2709@gmail.com>
2020-10-20 21:50:02 -0500

    figura geometrica

---
+++ src/main/java/sesion03/abstraccion/FiguraGeometrica.java
@@ -0,0 +1,29 @@
+package sesion03.abstraccion;
+
+public abstract class FiguraGeometrica {
+
+    protected String tipoFigura;
+
+    public FiguraGeometrica(String tipoFigura) {
+        this.tipoFigura = tipoFigura;
+    }
+
+    public String getTipoFigura() {
+        return tipoFigura;
+    }
+
+    public void setTipoFigura(String tipoFigura) {
+        this.tipoFigura = tipoFigura;
+    }
+
+    public abstract void dibujar();
+
+    public abstract void calcularArea();
+
+    @Override
+    public String toString() {
+        return "FiguraGeometrica{" +
+                "tipoFigura='" + tipoFigura + '\'' +
+                '}';
+    }
+}
